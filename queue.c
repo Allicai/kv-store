857fa5d10d5813de9ed051c8af662cd0ce018b17
Vinit Patel <patel.vinit@northeastern.edu>
2024-02-25 17:15:51 -0500

    mutex attempt

--- queue.c
+++ queue.c
@@ -5,13 +5,30 @@
 // TODO: define your synchronization variables here
 // (hint: don't forget to initialize them)
 
+static pthread_mutex_t queue_mux = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t queue_empty_cond = PTHREAD_MUTEX_INITIALIZER;
 
 // add a new request to the end of the queue
 // NOTE: queue must be implemented as a monitor
 void enqueue(queue_t *q, req_t *t) {
     /* TODO: your code here */
 
+    // locking the mutex before modifying queue
+    pthread_mutex_lock(&queue_mux);
 
+    if (q->head == NULL) { // if the queue is empty, we set the head and tail to the new req
+        q->head = t;
+        q->tail = t;
+    } else { // else we add the new req to the end of the queue
+        q->tail->next = t;
+        q->tail = t;
+    }
+
+    // signal that the queue isn't empty
+    pthread_cond_signal(&queue_empty_cond);
+
+    q->count++; // no. of requests + 1
+    pthread_mutex_unlock(&queue_mux); // unlock the mutex after modifying it
 }
 
 // pope a request from the head of the queue.
