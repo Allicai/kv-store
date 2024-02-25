21f67a172610415109caa411d11d42224a418027
Vinit Patel <patel.vinit@northeastern.edu>
2024-02-25 17:41:43 -0500

    thread-safe stats functionality

--- stats.c
+++ stats.c
@@ -16,40 +16,61 @@ int n_increases = 0;// number of increases
 // (hint: don't forget to initialize them)
 
 /* your code here */
+static pthread_mutex_t stats_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 // FIXME: implementation below is not thread-safe.
 // Fix this by implementing them as a Monitor.
 
 void inc_write() {
+    pthread_mutex_lock(&stats_mutex);
     n_writes++;
+    pthread_mutex_unlock(&stats_mutex);
 }
 
 void inc_read() {
+    pthread_mutex_lock(&stats_mutex);
     n_reads++;
+    pthread_mutex_unlock(&stats_mutex);
 }
 
 void inc_delete() {
+    pthread_mutex_lock(&stats_mutex);
     n_deletes++;
+    pthread_mutex_unlock(&stats_mutex);
 }
 
 void inc_increase() {
+    pthread_mutex_lock(&stats_mutex);
     n_increases++;
+    pthread_mutex_unlock(&stats_mutex);
 }
 
 
 int get_writes() {
-    return n_writes;
+    pthread_mutex_lock(&stats_mutex);
+    int writes = n_writes;
+    pthread_mutex_unlock(&stats_mutex);
+    return writes;
 }
 
 int get_reads() {
-    return n_reads;
+    pthread_mutex_lock(&stats_mutex);
+    int reads = n_reads;
+    pthread_mutex_unlock(&stats_mutex);
+    return reads;
 }
 
 int get_deletes() {
-    return n_deletes;
+    pthread_mutex_lock(&stats_mutex);
+    int deletes = n_deletes;
+    pthread_mutex_unlock(&stats_mutex);
+    return deletes;
 }
 
 int get_increases() {
-    return n_increases;
+    pthread_mutex_lock(&stats_mutex);
+    int increases = n_increases;
+    pthread_mutex_unlock(&stats_mutex);
+    return increases;
 }
